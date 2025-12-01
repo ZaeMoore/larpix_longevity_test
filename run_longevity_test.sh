86f280d4cf26f59e2b42a400849d3a4ce856cd50
Zae Moore <emoore06@syr.edu>
2025-12-01 14:27:52 -0500

    Longevity test script now runs step 1 and 2 at the correct times

--- run_longevity_test.sh
+++ run_longevity_test.sh
@@ -1,54 +1,128 @@
 #!/bin/bash
 
-# Set initial day
+#Start running the script and start counting t0
+#While t-t0<10min: run step 1
+#If t-t0>10min: run step 2
+#Perform failure test after step 1 and step 2. This will compare vddd, iddd, vdda, idda and pedestal of the last baseline file saved
+#Create a new subdirectory for each week to save data, once it becomes 7 days after initial day
+
+
+# Set initial day and time t0
 current_day=$(date +%Y-%m-%d)
+current_time=$(date +%H:%M)
+t0=$(date +%H:%M)
+d0=$(date +%Y-%m-%d)
+SECONDS=0
+echo -e "Starting longevity test script.\nCurrent day: $current_day. Current time: $current_time"
+
+t0_h=${t0:0:2}
+t0_m=${t0:3:2}
+t0_minutes=$(($t0_h * 60 + $t0_m))
+echo -e "t0 in minutes: $t0_minutes\n"
+
+
+#Figure out how to subtract times in bash
+#Date in seconds date +%s, use this to subtract dates
+#date +%M to get the minutes of the hour, date +%H to get the hour, turn the hour into minutes and add minutes, compare to t0
+
+#Create subfolders for each week to save data, then manually upload that week's folder into NERSC
 
 while true; do 
 
-    new_day=$(date +%Y-%m-%d)
+    current_day=$(date +%Y-%m-%d)
+    current_time=$(date +%H:%M)
+    echo "Current day: $current_day. Current time: $current_time"
 
-    # Wait until a new day is detected
-    if [["$new_day" != "$current_day"]]; then
+    current_time_h=${current_time:0:2}
+    current_time_m=${current_time:3:2}
+    current_time_minutes=$(($current_time_h * 60 + $current_time_m))
+    echo "Current time in minutes: $current_time_minutes"
 
-        echo "New day detected. Collecting baseline information."
+    time_difference=$(($current_time_minutes - $t0_minutes))
+    echo "Current time - t0 difference in minutes: $time_difference"
 
-        # Update the day
-        current_day="$new_day"
+    echo -e "Seconds since step 1 last finished: $SECONDS\n"
+
+    # If current time t-t0<10min, run step 1
+    # Main check
+    if [[ "$time_difference" -lt 10 ]]; then
+
+        echo "Collect baseline with nominal voltages"
 
         # Set 8 samples to nominal voltage
-        python3 power_on.py --vdda 51851 --pacman_tile 1,2,3,4,5,6,7,8
+        #python3 power_on.py --vdda 51851 --pacman_tile 1,2,3,4,5,6,7,8
+        echo "Power on script with nominal voltage"
 
         # Set boards to pedestal mode
-        python3 network_single_chip_pedestal.py --pacman_tile 1,2,3,4,5,6,7,8
+        #python3 network_single_chip_pedestal.py --pacman_tile 1,2,3,4,5,6,7,8
+        echo "Pedestal mode script"
 
         # Run baseline collection script. 
         # It collects vdda, idda, vddd, iddd and pedestal for all channels (daq=10min)
         # outloc options: 1=influxdb, 2=dictionary, 3=both
-        python3 measure_baseline.py --daq 600 --outloc 2
+        #python3 measure_baseline.py --daq 600 --outloc 2
+        echo "Baseline collection script (10 min)"
 
-        # For the rest pf the same day, run accelerated voltages test
-        while [[ "$(date +%Y-%m-%d)" == "$current_day"]]; do
+        # Check for failure
+        #python3 failure_check.py
+        echo -e "Failure check script\n"
 
-            # Set higher voltages, tile=1 is used as baseline with nominal voltage
-            python3 power_on.py --pacman_tile 2,3 --vdda <3.75V> --vddd <1.8V>
-            python3 power_on.py --pacman_tile 4,5 --vdda <4.38V> --vddd <2.10V>
-            python3 power_on.py --pacman_tile 6,7,8 --vdda <4.5V> --vddd <2.40V>
+        sleep 60
 
-            # Set boards to pedestal mode
-            pyhton3 network_single_chip_pedestal.py --pacman_tile 1,2,3,4,5,6,7,8
+        SECONDS=0
 
-            # Run baseline collection script
-            python3 measure_baseline.py --daq 600 --outloc 2
+    # If it has been more than 24 hours since step 1 last ended, run step 1 again
+    # Backup check 86400
+    elif [[ $SECONDS -gt 86400 ]]; then
 
-            # Check for failure
-            python3 failure_check.py
+        echo "Collect baseline with nominal voltages"
 
-        done
+        # Set 8 samples to nominal voltage
+        #python3 power_on.py --vdda 51851 --pacman_tile 1,2,3,4,5,6,7,8
+        echo "Power on script with nominal voltage"
 
+        # Set boards to pedestal mode
+        #python3 network_single_chip_pedestal.py --pacman_tile 1,2,3,4,5,6,7,8
+        echo "Pedestal mode script"
+
+        # Run baseline collection script. 
+        # It collects vdda, idda, vddd, iddd and pedestal for all channels (daq=10min)
+        # outloc options: 1=influxdb, 2=dictionary, 3=both
+        #python3 measure_baseline.py --daq 600 --outloc 2
+        echo "Baseline collection script (10 min)"
+
+        # Check for failure
+        #python3 failure_check.py
+        echo -e "Failure check script\n"
+
+        sleep 60
+
+        SECONDS=0
+    
     else
 
-        # Still the same day, keep sleeping until the day changes
-        sleep 60 # check once per minute
+        echo "Collect baseline with accelerated voltages"
+
+        # If current time t-t0>10min, run step 2
+        # Set higher voltages, tile=1 is used as baseline with nominal voltage
+        #python3 power_on.py --pacman_tile 2,3 --vdda <3.75V> --vddd <1.8V>
+        #python3 power_on.py --pacman_tile 4,5 --vdda <4.38V> --vddd <2.10V>
+        #python3 power_on.py --pacman_tile 6,7,8 --vdda <4.5V> --vddd <2.40V>
+        echo "Power on script with accelerated voltages"
+
+        # Set boards to pedestal mode
+        #python3 network_single_chip_pedestal.py --pacman_tile 1,2,3,4,5,6,7,8
+        echo "Pedestal mode script"
+
+        # Run baseline collection script
+        #python3 measure_baseline.py --daq 600 --outloc 2
+        echo "Baseline collection script (10 min)"
+
+        # Check for failure
+        #python3 failure_check.py
+        echo -e "Failure check script\n"
+
+        sleep 60
 
     fi 
 
