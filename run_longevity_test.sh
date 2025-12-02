212f6b32679f3e08e98c634c6cf0a7fa909c1c77
Zae Moore <emoore06@syr.edu>
2025-12-02 13:25:37 -0500

    Directory creation

--- run_longevity_test.sh
+++ run_longevity_test.sh
@@ -18,11 +18,11 @@ echo -e "Starting longevity test script.\nCurrent day: $current_day. Current tim
 
 t0_h=${t0:0:2}
 t0_m=${t0:3:2}
-t0_minutes=$((10#$t0_h * 60.0 + 10#$t0_m))
+t0_minutes=$((10#$t0_h * 60 + 10#$t0_m))
 echo -e "t0 in minutes: $t0_minutes\n"
 
-w0_s= $(date +%s) #Initial time in seconds for week 0
-#Creat initial subdirectory for week 0
+w0_s=$(date +%s) #Initial time in seconds for week 0
+mkdir "su_cryolongevity_$(date +%Y-%m-%d)"
 
 while true; do 
 
@@ -32,7 +32,7 @@ while true; do
 
     current_time_h=${current_time:0:2}
     current_time_m=${current_time:3:2}
-    current_time_minutes=$((10#$current_time_h * 60.0 + 10#$current_time_m))
+    current_time_minutes=$((10#$current_time_h * 60 + 10#$current_time_m))
     echo "Current time in minutes: $current_time_minutes"
 
     time_difference=$((10#$current_time_minutes - 10#$t0_minutes))
@@ -48,7 +48,8 @@ while true; do
         #When it's been 604800 seconds since initial time, create a new subdirectory for that week
         #Reset week_0 time (in seconds) to current time
         #
-        if [[ 10#$(date +%s) - 10#$w0_s -gt 604800.0 ]]; then
+        week_difference=$((10#$(date +%s) - 10#$w0_s))
+        if [[ 10#$week_difference -gt 604800 ]]; then
             w0_s=$(date +%s)
             echo "Creating new subdirectory for the week"
             mkdir "su_cryolongevity_$(date +%Y-%m-%d)"
