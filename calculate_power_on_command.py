7b221a86f790fc6a5dc51cfa9091c4ae4d07b4e3
Zae Moore <emoore06@syr.edu>
2025-12-12 14:57:36 -0500

    Fixed list of healthy tiles

--- calculate_power_on_command.py
+++ calculate_power_on_command.py
@@ -31,29 +31,29 @@ def main(healthy, mode):
         Comma-separated string of vddd values
     """
 
+    # list of [tile,vdda,vddd]
+    list_tiles = [
+        [1, 2.5, 1.2],
+        [2, 2.9, 1.38],
+        [3, 2.9, 1.38],
+        [4, 3.2, 1.55],
+        [5, 3.2, 1.55],
+        [6, 3.6, 1.73],
+        [7, 3.6, 1.73],
+        [8, 3.6, 1.73]
+    ]
+
     if mode == 'nominal':
         string_tile_number = ''
         string_vdda = '5243'
         string_vddd = '26214'
 
-        for index, tile_number in enumerate(healthy):
-            string_tile_number += f'{list_tiles[index][0]},'
+        for tile_number in healthy:
+            string_tile_number += f'{tile_number},'
 
         return string_tile_number[:-1], string_vdda, string_vddd
     
     if mode == 'accelerated':
-
-        # list of [tile,vdda,vddd]
-        list_tiles = [
-            [1, 2.5, 1.2],
-            [2, 2.9, 1.38],
-            [3, 2.9, 1.38],
-            [4, 3.2, 1.55],
-            [5, 3.2, 1.55],
-            [6, 3.6, 1.73],
-            [7, 3.6, 1.73],
-            [8, 3.6, 1.73]
-        ]
 
         def calc_vddd(VDDD):
             return ((VDDD-0.2)/2.5) * 65536
@@ -68,15 +68,15 @@ def main(healthy, mode):
         string_vdda = ''
         string_vddd = ''
 
-        for index, tile_number in enumerate(healthy):
+        for tile_number in healthy:
             # Array format
-            list_tile_number.append(list_tiles[index][0])
-            list_vdda.append(calc_vdda(list_tiles[index][1]))
-            list_vddd.append(calc_vddd(list_tiles[index][2]))
+            list_tile_number.append(list_tiles[tile_number-1][0])
+            list_vdda.append(calc_vdda(list_tiles[tile_number-1][1]))
+            list_vddd.append(calc_vddd(list_tiles[tile_number-1][2]))
             # String format
-            string_tile_number += f'{list_tiles[index][0]},'
-            string_vdda += f'{int(calc_vdda(list_tiles[index][1]))},'
-            string_vddd += f'{int(calc_vddd(list_tiles[index][2]))},'
+            string_tile_number += f'{list_tiles[tile_number-1][0]},'
+            string_vdda += f'{int(calc_vdda(list_tiles[tile_number-1][1]))},'
+            string_vddd += f'{int(calc_vddd(list_tiles[tile_number-1][2]))},'
 
         return string_tile_number[:-1], string_vdda[:-1], string_vddd[:-1]
 
